d4925dd675963adba46a9cd68a8f48f2e56e5799
stefan <ste_hof@uni-bremen.de>
2020-08-08 20:14:31 +0200

    added print for the top 5 changers with interday info

--- trade.py
+++ trade.py
@@ -1,10 +1,11 @@
 from scrape import get_daily_top_n
 from alpha_vantage.timeseries import TimeSeries
-"""
+
 key = "RROECGAUHBTD2S5B"
 
+top_5  = get_daily_top_n(5)
+
 ts = TimeSeries(key=key)
-aapl, meta = ts.get_intraday(symbol='AAPL')
-print(aapl)
-"""
-print(get_daily_top_n(5))
+for name in top_5:
+    data, meta = ts.get_intraday(symbol=name)
+    print(data)
