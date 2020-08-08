9a7fafb4e16987f0f6467ed802776570252b451d
stefan <ste_hof@uni-bremen.de>
2020-08-08 20:10:31 +0200

    scrape now gets the top n changers for the day and trade can call them.

--- scrape.py
+++ scrape.py
@@ -1,9 +1,13 @@
 import requests
-import bs4 as bs
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 
-URL = 'https://finance.yahoo.com/screener/predefined/day_gainers?guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJPzhFJgI--8KNKwgdiM8Kk7WMl_EHqQdXXO5CTmur7k9dFFg15hppLMOhEDIO1kXDNbZHUeWbHd_C0YlFu7OQAvpyolavIM_C0mLJMi0KYNalp-jWYFz70rGmTjS96gm-8ZuMwPME_JOKIJPtZdeTnDHaBUvfar2oqZfEND0wIl&_guc_consent_skip=1596900194'
-page = requests.get(URL)
+def get_daily_top_n (top_n):
+    URL = 'https://finance.yahoo.com/screener/predefined/day_gainers?guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJPzhFJgI--8KNKwgdiM8Kk7WMl_EHqQdXXO5CTmur7k9dFFg15hppLMOhEDIO1kXDNbZHUeWbHd_C0YlFu7OQAvpyolavIM_C0mLJMi0KYNalp-jWYFz70rGmTjS96gm-8ZuMwPME_JOKIJPtZdeTnDHaBUvfar2oqZfEND0wIl&_guc_consent_skip=1596900194'
+    page = requests.get(URL)
 
-soup =  BeautifulSoup(page.content, 'html.parser')
+    df_list = pd.read_html(page.text)
+    df = df_list[0]
+    return(df['Symbol'][:top_n])
--- trade.py
+++ trade.py
@@ -1,10 +1,10 @@
-import yfinance as yf
+from scrape import get_daily_top_n
 from alpha_vantage.timeseries import TimeSeries
-
+"""
 key = "RROECGAUHBTD2S5B"
 
 ts = TimeSeries(key=key)
 aapl, meta = ts.get_intraday(symbol='AAPL')
 print(aapl)
-
-
+"""
+print(get_daily_top_n(5))
