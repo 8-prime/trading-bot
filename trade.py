91d3d070d4dc72ca08e5ef8472e066b1e2385192
stefan <ste_hof@uni-bremen.de>
2020-08-09 20:56:35 +0200

    rough outlining of what the shit im acutally planning to do here. methdos to check whether a stock should be sold as well as if a stock should be bought(no clue how to determine that ) also main programm running in while loop -> find a better way later on to be able to check the currently held money and shizzle probbs wanna offload a whole host of info as to not rely on the programm to never crash

--- trade.py
+++ trade.py
@@ -8,22 +8,46 @@ ts = TimeSeries(key=key)
 #retrieve top n changers for the day
 top_5  = get_daily_top_n(5)
 
-
-for name in top_5:
-    data, meta = ts.get_intraday(symbol=name)
-    stonks += [data]
-    print(data[0])
-
 #handling the amount of held stocks and the money
 money = 1000
 held_stocks = []
 
-def sell(name_of_stock):
+def sell(stock):
     #sell remove stock from the list of held stocks and add the money the amount of stocks are worth to the money
     return 0
 
-def buy(name_of_stock):
+def buy(stock):
     #add amount of bought stocks to the list of held stocks and remove the cost from money
     return 0
 
+#return true if a stock is worth buying
+def check_stock_buyabulity():
+    return True
 
+#check if the maximum stop loss has been reached or if the stock fell under the daily average
+def check_stock_sellability(stock):
+    return True
+
+
+#this is where the program will be most of the time
+while(True):
+    #check if i have to sell stocks because their value is decreasing
+    for stock in held_stocks:
+        if check_stock_sellability(stock):
+            sell(stock)
+    #first check of there is money to spend
+    #price / 5-i
+    for name in top_5:
+        #if miney / 5-i is greater than the price of the stock, fecking go mate
+        #if we already own stock for this changer, we will check the next one and so on, potentially not buying at all
+
+
+    
+
+
+"""
+for name in top_5:
+    data, meta = ts.get_intraday(symbol=name)
+    stonks += [data]
+    print(data[0])
+"""
