d096d51bf7cbd5fc53abde80f295ae01aaf11af3
stefan <ste_hof@uni-bremen.de>
2020-08-09 13:09:54 +0200

    added some more function and added class for stocks to handle pricing and amount held and such

---
+++ stock.py
@@ -0,0 +1,6 @@
+
+class Stock:
+
+    price_time_of_purchase = 0
+    sliding_average = 0
+    
--- trade.py
+++ trade.py
@@ -1,11 +1,29 @@
 from scrape import get_daily_top_n
 from alpha_vantage.timeseries import TimeSeries
 
+#setup for using alpha vantage for getting currenty prices for stocks
 key = "RROECGAUHBTD2S5B"
+ts = TimeSeries(key=key)
 
+#retrieve top n changers for the day
 top_5  = get_daily_top_n(5)
 
-ts = TimeSeries(key=key)
+
 for name in top_5:
     data, meta = ts.get_intraday(symbol=name)
-    print(data)
+    stonks += [data]
+    print(data[0])
+
+#handling the amount of held stocks and the money
+money = 1000
+held_stocks = []
+
+def sell(name_of_stock):
+    #sell remove stock from the list of held stocks and add the money the amount of stocks are worth to the money
+    return 0
+
+def buy(name_of_stock):
+    #add amount of bought stocks to the list of held stocks and remove the cost from money
+    return 0
+
+
